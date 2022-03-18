d25c5d744e1cc570c24c80f4a2c174527d7b7735
nastiang <38843426+nastiang@users.noreply.github.com>
2022-03-18 08:56:16 +0300

    update child, added approval func

--- contracts/Child.sol
+++ contracts/Child.sol
@@ -16,17 +16,21 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Child is Ownable, IERC721Receiver{
     address mintAddress;
     address reciever; 
-    uint mintCost = 10000 gwei;
+    uint mintCost;
     uint mintAmount;  // num of mint for each address
+    uint256[] tokenIds;
     IERC721 public parentNFT;
     constructor(address _mintAddress,uint _mintAmount, uint _mintCost, address _reciever){
        mintAddress = _mintAddress;
        reciever = _reciever;
        mintAmount = _mintAmount;
        parentNFT = IERC721(_mintAddress); 
+       mintCost = _mintCost;
     }
 
     fallback() payable external {}
+
+    event nftIdAdded(uint id);
 
       function onERC721Received(
         address operator, 
@@ -34,8 +38,13 @@ contract Child is Ownable, IERC721Receiver{
         uint256 tokenId, 
         bytes calldata data)  public virtual override returns (bytes4) {   
         return this.onERC721Received.selector;
-        
+        tokenIds.push(tokenId);
+        emit nftIdAdded(tokenId);
     } 
+
+    function getTokens() external view returns(uint256[] memory _tokens){
+       _tokens = tokenIds;
+     }  
 
 event BuyMint(bool status, bytes result);
     
@@ -43,20 +52,19 @@ event BuyMint(bool status, bytes result);
     external
     payable onlyOwner
 {
-    bool status;
-    bytes memory result;
-    //change this line depending on the contract being called
-   // (bool success, bytes memory returnData) = mintAddress.call{value:mintCost, gas: 3000000}(abi.encodeWithSignature("mint(uint256)", mintAmount));
-    (bool success, bytes memory returnData) = mintAddress.call{value:mintCost, gas: 3000000}(abi.encodeWithSignature("mint()")); 
-     
+    uint totalCost = mintCost * mintAmount;
+    //for(uint i=0;i<mintAmount; i++){
+    (bool success, bytes memory returnData) = mintAddress.call{value:totalCost, gas: 3000000}(abi.encodeWithSignature("mint(uint256)", mintAmount));
+    // (status, result) = mintAddress.delegatecall(abi.encodePacked(bytes4(keccak256(abi.encodePacked("mint(uint256)"))), mintAmount));
+   // require(success);
     emit BuyMint(success, returnData);
- 
+    
+    parentNFT.setApprovalForAll(owner(), true);
 }
 
-    function transferToDad(uint256[] memory _ids) external onlyOwner {
-          for(uint i=0;i<_ids.length; i++){
-             parentNFT.transferFrom(address(this), reciever, i);
-       }
+    function transferToDad(uint _tokenId) external onlyOwner {
+             parentNFT.transferFrom(address(this), reciever, _tokenId);
+             parentNFT.setApprovalForAll(reciever, true);
     }
 
     function destroyContract() external onlyOwner {
